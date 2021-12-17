321b3d4cf9efa865809480980cc3d950d7bb6634
Kehao Guo <kg2937@columbia.edu>
2021-12-17 03:33:12 -0500

    use pandas-gbq

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ import yfinance as yf
 import plotly.graph_objects as go
 from google.oauth2 import service_account
 from google.cloud import bigquery
+import pandas_gbq
+
 
 # API client
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
-client = bigquery.Client(credentials=credentials)
+# client = bigquery.Client(credentials=credentials)
 
 st.title('Stock Price Prediction')
 
-# stock = st.text_input('Enter stock ticker:', placeholder='AAPL')
 stock = st.selectbox(
     'Select a stock:',
     ('AAPL (Apple)', 'TSLA (Tesla)', 'MSFT (Microsoft)', 'AMZN (Amazon)', 'GOOGL (Google)')
@@ -27,7 +28,7 @@ five_days_before = today - timedelta(days=5)
 five_years_before = today - timedelta(days=1825)
 start_date = st.date_input('Enter start date:', five_days_before, max_value=today-timedelta(days=1), min_value=five_years_before)
 end_date = st.date_input('Enter end date:', today, max_value=today, min_value=five_years_before+timedelta(days=60))
-# predict for x days future INSTEAD OF a single day???
+
 
 # load stock price from yahoo
 @st.cache(allow_output_mutation=True, ttl=600)
@@ -47,13 +48,14 @@ if st.checkbox('Show stock price dataframe'):
 
 #TODO: load predicted data
 @st.cache(allow_output_mutation=True, ttl=600)
-def query_predicted(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    rows = [dict(row) for row in rows_raw]
-    return rows
+def make_query(query):
+    # query_job = client.query(query)
+    # rows_raw = query_job.result()
+    # rows = [dict(row) for row in rows_raw]
+    # return rows
+    pandas_gbq.read_gbq(query, project_id='sublime-cargo-326805', credentials=credentials)
 
-pred = query_predicted(
+pred = make_query(
     "SELECT Date, Predictions FROM `sublime-cargo-326805.stockPrediction.prediction` WHERE Date between '{}' AND '{}' ORDER BY Date;".format(start_date, end_date)
 )
 pred_df = pd.DataFrame.from_dict(pred, orient='index')
