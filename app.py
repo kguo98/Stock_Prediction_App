d426e4cdc41acf2b13d68e146283e731a4c5f108
Kehao Guo <kg2937@columbia.edu>
2021-12-17 02:23:01 -0500

    ff

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import plotly.graph_objects as go
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-
 # API client
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
